89d4381f6f5d2c5a165ef7f2004544e1bb064399
NaveenrajV <naveenraj436@gmail.com>
2019-09-10 11:27:46 +0530

    Description added

--- news/src/components/News/News.js
+++ news/src/components/News/News.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const News = (props) => {
-  console.log(props)
   const {name,description,url,category,country} = props.selectedSource
   return (
     <div>
       <h2>{name}</h2>
+      <p><strong>Category :</strong>{category}</p>
       <p><strong>Description : </strong>{description}</p>
     </div>
   )
